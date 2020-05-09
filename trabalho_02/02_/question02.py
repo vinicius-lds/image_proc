6412e76e4604c2201d64d63907f06afccad69f84
Vinícius Luis da Silva <vinicius.lds.br@gmail.com>
2020-05-09 20:19:39 -0300

    alterações

--- trabalho_02/02_/question02.py
+++ trabalho_02/02_/question02.py
@@ -7,12 +7,12 @@ from matplotlib import pyplot as plt
 def process(img):
     gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     negative = 255 - gray_scale
-    sei_la = negative + 50
-    _, binary = cv2.threshold(negative, 170, 255, cv2.THRESH_BINARY)    
-    sl = cv2.morphologyEx(binary, cv2.MORPH_TOPHAT, np.ones((30, 30)))
+    pls50 = negative + 50
+    _, binary = cv2.threshold(pls50, 170, 255, cv2.THRESH_BINARY)    
+    top_hat = cv2.morphologyEx(binary, cv2.MORPH_TOPHAT, np.ones((30, 30)))
 
     plt.figure(1)
-    plt.imshow(sl, cmap='gray')
+    plt.imshow(top_hat, cmap='gray')
 
     plt.show()
     
